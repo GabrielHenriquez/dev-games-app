8569a18406c0929dbc1bfc7a6e3f282fe1bc4ef5
Gabriel Henrique <estudogbzs@gmail.com>
2024-06-30 18:08:24 -0300

    :bug: fix(svg): config to show images svg

--- metro.config.js
+++ metro.config.js
@@ -1,22 +1,23 @@
-const { getDefaultConfig } = require("expo/metro-config");
+const { getDefaultConfig } = require("@expo/metro-config");
 const { withNativeWind } = require("nativewind/metro");
 
 metroConfig = () => {
-  const config = getDefaultConfig(__dirname, { isCSSEnabled: true });
+  const config = getDefaultConfig(__dirname);
+  config.resolver.sourceExts.push("cjs");
+
   const { transformer, resolver } = config;
 
   config.transformer = {
     ...transformer,
-    babelTransoformPath: require.resolve("react-native-svg-transformer"),
+    babelTransformerPath: require.resolve("react-native-svg-transformer"),
   };
-
   config.resolver = {
     ...resolver,
     assetExts: resolver.assetExts.filter((ext) => ext !== "svg"),
     sourceExts: [...resolver.sourceExts, "svg", "ts", "tsx"],
   };
 
-  return withNativeWind(config, { input: "./global.css" });
+  return withNativeWind(config, { input: "./src/common/styles/global.css" });
 };
 
 module.exports = metroConfig;
