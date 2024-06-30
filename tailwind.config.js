e987c597119aa601672663d453208b564d73499a
Gabriel Henrique <estudogbzs@gmail.com>
2024-06-29 23:18:25 -0300

    :wrench: chore(styles): config some nativewind styles
    - config some styles in the nativewind extended theme, such as font and
      some colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,19 @@
+import { colors } from "./src/common/styles/Colors";
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./app/**/*.{js,jsx,ts,tsx}"],
-    presets: [require("nativewind/preset")],
-    theme: {
-        extend: {},
+  content: ["./app/**/*.{js,jsx,ts,tsx}"],
+  presets: [require("nativewind/preset")],
+  theme: {
+    extend: {
+      colors: colors,
+      fontfamily: {
+        regular: "Sora_400Regular",
+        medium: "Sora_500Medium",
+        semiBold: "Sora_600SemiBold",
+        bold: "Sora_700Bold",
+      },
     },
-    plugins: [],
-}
+  },
+  plugins: [],
+};
