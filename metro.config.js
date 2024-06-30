8d6147b25cdba7ebecaecf8608744f2001579415
Gabriel Henrique <estudogbzs@gmail.com>
2024-06-29 22:30:04 -0300

    :wrench: chore(svg): config svg in the project
    - change metro.config.js to transformer and resolver svg and add declare
      module to svg

--- metro.config.js
+++ metro.config.js
@@ -1,6 +1,22 @@
 const { getDefaultConfig } = require("expo/metro-config");
-const { withNativeWind } = require('nativewind/metro');
+const { withNativeWind } = require("nativewind/metro");
 
-const config = getDefaultConfig(__dirname, { isCSSEnabled: true })
+metroConfig = () => {
+  const config = getDefaultConfig(__dirname, { isCSSEnabled: true });
+  const { transformer, resolver } = config;
 
-module.exports = withNativeWind(config, { input: './global.css' })
+  config.transformer = {
+    ...transformer,
+    babelTransoformPath: require.resolve("react-native-svg-transformer"),
+  };
+
+  config.resolver = {
+    ...resolver,
+    assetExts: resolver.assetExts.filter((ext) => ext !== "svg"),
+    sourceExts: [...resolver.sourceExts, "svg", "ts", "tsx"],
+  };
+
+  return withNativeWind(config, { input: "./global.css" });
+};
+
+module.exports = metroConfig;
