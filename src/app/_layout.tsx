22eca0ee0d15de41247640f4ce5a28247ddacaa9
Gabriel Henrique <estudogbzs@gmail.com>
2024-06-29 22:16:06 -0300

    :heavy_plus_sign: chore(fonts): add font and config in the project

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,8 +1,28 @@
 import { Stack } from "expo-router";
+import "../../global.css";
+import * as SplashScreen from "expo-splash-screen";
+import {
+  useFonts,
+  Sora_400Regular,
+  Sora_500Medium,
+  Sora_600SemiBold,
+  Sora_700Bold,
+} from "@expo-google-fonts/sora";
 
-import "../global.css"
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
+  const [fontsLoaded] = useFonts({
+    Sora_400Regular,
+    Sora_500Medium,
+    Sora_600SemiBold,
+    Sora_700Bold,
+  });
+
+  if (fontsLoaded) {
+    SplashScreen.hideAsync();
+  }
+
   return (
     <Stack>
       <Stack.Screen name="index" />
