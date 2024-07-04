41b74d18195622ac5a70b9998b11da80d69bf807
Gabriel Henrique <estudogbzs@gmail.com>
2024-07-03 22:26:20 -0300

    :wrench: chore: add new types and path config

--- babel.config.js
+++ babel.config.js
@@ -1,22 +1,22 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: [
-      ["babel-preset-expo", { jsxImportSource: "nativewind" }],
-      "nativewind/babel",
-    ],
+    presets: [['babel-preset-expo', { jsxImportSource: 'nativewind' }], 'nativewind/babel'],
     plugins: [
-      "react-native-reanimated/plugin",
+      'react-native-reanimated/plugin',
       [
-        "module-resolver",
+        'module-resolver',
         {
           alias: {
-            "@assets": "./src/assets",
-            "@components": "./src/common/components",
-            "@config": "./src/common/config",
-            "@styles": "./src/common/styles",
+            '@assets': './src/assets',
+            '@components': './src/common/components',
+            '@config': './src/common/config',
+            '@styles': './src/common/styles',
+            '@stores': './src/common/stores',
+            '@services': './src/common/services',
+            '@hooks': './src/common/hooks',
           },
-          extensions: [".js", ".jsx", ".ts", ".tsx", ".json"],
+          extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
         },
       ],
     ],
