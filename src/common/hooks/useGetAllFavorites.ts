fb6d4b9627f6ca05902806b2562739ddaf2872c8
Gabriel Henrique <estudogbzs@gmail.com>
2024-07-03 21:53:14 -0300

    :sparkles: feat(hook): new hook to get favorites

---
+++ src/common/hooks/useGetAllFavorites.ts
@@ -0,0 +1,20 @@
+import { useQuery } from '@tanstack/react-query';
+import { IGameDetails } from '@services/games_services';
+import { storage } from 'storage/mmkv';
+
+export const useGetAllFavorites = () => {
+  const query = useQuery<IGameDetails[]>({
+    queryKey: ['getAllFavorites'],
+    queryFn: () => {
+      const RESULT_STRING = storage.getString('favorites');
+      if (RESULT_STRING) {
+        const RESULT = JSON.parse(RESULT_STRING);
+        return RESULT.reverse();
+      } else {
+        return [];
+      }
+    },
+  });
+
+  return query;
+};
