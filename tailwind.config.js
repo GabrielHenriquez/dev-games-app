32b7308999d15528f7b19457b8f48aaa3a8109ae
Gabriel Henrique <estudogbzs@gmail.com>
2024-07-03 22:25:14 -0300

    :wrench: chore: add permissions backgroundOpacity

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,11 @@
 import { colors } from './src/common/styles/colors';
 import { fontsFamily } from './src/common/styles/fonts';
 
+const content = ['./src/app/**/*.{js,jsx,ts,tsx}', './src/common/components/**/*.{js,jsx,ts,tsx}'];
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/app/**/*.{js,jsx,ts,tsx}', './src/common/components/**/*.{js,jsx,ts,tsx}'],
+  content,
   presets: [require('nativewind/preset')],
   theme: {
     extend: {
@@ -12,4 +14,7 @@ module.exports = {
     },
   },
   plugins: [],
+  corePlugin: {
+    backgroundOpacity: true,
+  },
 };
