b074de61a36e6a323115479b46908b3341c0e88c
Gabriel Henrique <estudogbzs@gmail.com>
2024-07-03 22:07:58 -0300

    :sparkles: feat: add roundedButton trash to remove

--- src/common/components/GameList.tsx
+++ src/common/components/GameList.tsx
@@ -1,17 +1,53 @@
-import { IGame } from 'common/services/game_services/models';
+import { DataTypeScreen } from 'app/Games/[param]';
 import GameCard from './GameCard';
+import { IGame, IGameDetails } from 'common/services/games_services';
+import { useRouter } from 'expo-router';
+import RoundedButton from './RoundedButton';
+import IconTrash from '@assets/icons/trash.svg';
 import * as Native from 'react-native';
+import { useFavorites } from '@hooks/useFavorites';
 
-const GameList = ({ data }: { data: IGame[] }) => (
-  <Native.View className="flex-1 px-3">
-    <Native.FlatList
-      data={data}
-      showsVerticalScrollIndicator={false}
-      keyExtractor={(item) => item?.id.toString()}
-      renderItem={({ item }) => <GameCard data={item} />}
-      contentContainerClassName="gap-4 pb-4"
-    />
-  </Native.View>
-);
+const GameList = ({
+  data,
+  screenType,
+}: {
+  data: IGame[] | IGameDetails[];
+  screenType?: DataTypeScreen;
+}) => {
+  const { push } = useRouter();
+  const navigateGameDetails = (slug: string) => {
+    push(`/GameDetails/${slug}`);
+  };
+  const { removeFromFavorites } = useFavorites();
+
+  const handleRemoveFromFavorites = (item: IGame | IGameDetails) => {
+    if ('description_raw' in item) removeFromFavorites(item as IGameDetails);
+  };
+
+  return (
+    <Native.View className="flex-1 px-3">
+      <Native.FlatList
+        data={data}
+        showsVerticalScrollIndicator={false}
+        keyExtractor={(item) => item?.id.toString()}
+        contentContainerClassName="pb-4"
+        contentContainerStyle={{ gap: 17 }}
+        renderItem={({ item }: { item: IGame | IGameDetails }) => (
+          <>
+            <GameCard onPress={() => navigateGameDetails(item?.slug)} data={item} />
+            {screenType === 'Favorites' && (
+              <RoundedButton
+                style={{ position: 'absolute', right: 12, top: 12 }}
+                type={'actionButton'}
+                onPress={() => handleRemoveFromFavorites(item)}>
+                <IconTrash />
+              </RoundedButton>
+            )}
+          </>
+        )}
+      />
+    </Native.View>
+  );
+};
 
 export default GameList;
